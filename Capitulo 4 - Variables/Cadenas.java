7c81106d138a81c3b4bee7e9522a4222bf315bd8
RobertoGuerreroBueno <roberto.guebu90@gmail.com>
2018-05-19 23:46:37 -0500

    Manejo básico de cadenas

---
+++ Capitulo 4 - Variables/Cadenas.java
@@ -0,0 +1,13 @@
+public class Cadenas{
+	public static void main(String[] args){
+		String nombre = "Roberto";
+		String apellido = "Guerrero";
+		System.out.println("Concatenacion: "+nombre+apellido);
+		System.out.println("Nueva linea: \n"+nombre+" "+apellido);
+		System.out.println("Tabulador: \t"+nombre+" "+apellido);
+		System.out.println("Retroceso: \b"+nombre+" "+apellido);
+		System.out.println("Retorno de carro: \r"+nombre+" "+apellido);
+		System.out.println("Comilla simple: \'"+nombre+" "+apellido+"\'");
+		System.out.println("Comilla doble: \""+nombre+" "+apellido+"\"");
+	}
+}
